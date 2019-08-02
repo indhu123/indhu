6d9052d898bbdde6ff5729541a570a420523c3a8
indhu123 <53117685+indhu123@users.noreply.github.com>
2019-08-02 22:22:52 +0530

    Add files via upload

---
+++ prime number between interval.py
@@ -0,0 +1,8 @@
+n,q=map(int,input().split())
+for num in range(n+1,q):
+        if num>1:
+            for i in range(2,num):
+                if num%i==0:
+                    break
+            else:
+                print(num,end=" ")
